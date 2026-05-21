b480de8b3f5b4dfb9492879e716ab1c08e38f915
Gideon Okorie <giditech14.com>
2026-05-21 19:51:42 +0100

    written the typescript tests, all passing

--- vault/tests/vault.ts
+++ vault/tests/vault.ts
@@ -1,7 +1,12 @@
 import * as anchor from "@anchor-lang/core";
 import { Program } from "@anchor-lang/core";
 import { Vault } from "../target/types/vault";
-import { Commitment, LAMPORTS_PER_SOL, PublicKey } from "@solana/web3.js";
+import {
+  Commitment,
+  LAMPORTS_PER_SOL,
+  PublicKey,
+  SystemProgram,
+} from "@solana/web3.js";
 import NodeWallet from "@anchor-lang/core/dist/cjs/nodewallet";
 import { BN } from "bn.js";
 import { assert, expect } from "chai";
@@ -46,5 +51,109 @@ describe("vault", () => {
       10 * LAMPORTS_PER_SOL
     );
     await confirmTx(sig);
+  });
+
+  it("Initialized the vault", async () => {
+    const tx = await program.methods
+      .initialize()
+      .accountsStrict({
+        user: user,
+        vaultState: vaultStatePda,
+        vault: vaultPda,
+        systemProgram: SystemProgram.programId,
+      })
+      .rpc();
+
+    await confirmTx(tx);
+
+    const vaultState = await program.account.vaultState.fetch(vaultStatePda);
+    expect(vaultState.vaultBump).to.equal(vaultBump);
+    expect(vaultState.stateBump).to.equal(stateBump);
+  });
+
+  it("Deposit 1 SOL into the vault", async () => {
+    const depositAmount = 1 * LAMPORTS_PER_SOL;
+
+    const initialVaultBalance = await provider.connection.getBalance(vaultPda);
+    const initialUserBalance = await provider.connection.getBalance(user);
+
+    const tx = await program.methods
+      .deposit(new BN(depositAmount))
+      .accountsStrict({
+        user: user,
+        vaultState: vaultStatePda,
+        vault: vaultPda,
+        systemProgram: SystemProgram.programId,
+      })
+      .rpc();
+
+    await confirmTx(tx);
+
+    const finalBalanceVault = await provider.connection.getBalance(vaultPda);
+    const finalBalanceUser = await provider.connection.getBalance(user);
+
+    console.log("Initial Vault Balance:", initialVaultBalance);
+    console.log("Final Vault Balance:", finalBalanceVault);
+
+    expect(finalBalanceVault).to.equal(initialVaultBalance + depositAmount);
+    expect(finalBalanceUser).to.be.lessThan(initialUserBalance - depositAmount);
+  });
+
+  it("Withdraw 0.5 SOL from the vault", async () => {
+    const withdrawAmount = 0.5 * LAMPORTS_PER_SOL;
+
+    const initialVaultBalance = await provider.connection.getBalance(vaultPda);
+    const initialUserBalance = await provider.connection.getBalance(user);
+
+    const tx = await program.methods
+      .withdraw(new BN(withdrawAmount))
+      .accountsStrict({
+        user: user,
+        vaultState: vaultStatePda,
+        vault: vaultPda,
+        systemProgram: SystemProgram.programId,
+      })
+      .rpc();
+
+    await confirmTx(tx);
+
+    const finalBalanceVault = await provider.connection.getBalance(vaultPda);
+    const finalBalanceUser = await provider.connection.getBalance(user);
+
+    console.log(
+      "Initial Vault Balance before withdrawal:",
+      initialVaultBalance
+    );
+    console.log("Final Vault Balance after withdrawal:", finalBalanceVault);
+
+    expect(finalBalanceVault).to.equal(initialVaultBalance - withdrawAmount);
+    expect(finalBalanceUser).to.be.greaterThan(initialUserBalance);
+  });
+
+  it("Close the vault and transfer all funds to the user", async () => {
+    const initialUserBalance = await provider.connection.getBalance(user);
+
+    const tx = await program.methods
+      .close()
+      .accountsStrict({
+        user: user,
+        vaultState: vaultStatePda,
+        vault: vaultPda,
+        systemProgram: SystemProgram.programId,
+      })
+      .rpc();
+
+    confirmTx(tx);
+
+    expect(await provider.connection.getBalance(vaultPda)).to.equal(0);
+
+    const vaultStateInfo = await provider.connection.getAccountInfo(
+      vaultStatePda
+    );
+    expect(vaultStateInfo).to.be.null;
+
+    const finalBalanceUser = await provider.connection.getBalance(user);
+
+    expect(finalBalanceUser).to.be.greaterThan(initialUserBalance);
   });
 });
