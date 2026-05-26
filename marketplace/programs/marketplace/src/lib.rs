a9d63a3da6f6c83d0167dbad94c15372f3acda61
Gideon Okorie <giditech14.com>
2026-05-26 13:16:54 +0100

    started building an nft marketplace

---
+++ marketplace/programs/marketplace/src/lib.rs
@@ -0,0 +1,21 @@
+pub mod constants;
+pub mod error;
+pub mod instructions;
+pub mod state;
+
+use anchor_lang::prelude::*;
+
+pub use constants::*;
+pub use instructions::*;
+pub use state::*;
+
+declare_id!("58G2AW7qke8nEPsL8oVqPMwuetFfR2cbxD1XTZ8XFCcT");
+
+#[program]
+pub mod marketplace {
+    use super::*;
+
+    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+        initialize::handler(ctx)
+    }
+}
