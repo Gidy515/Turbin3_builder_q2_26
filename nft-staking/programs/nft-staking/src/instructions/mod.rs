9d72529259c77f8a6459d06f4e6363b71294aa61
Gideon Okorie <giditech14.com>
2026-06-05 17:39:20 +0100

    built nft staking program

---
+++ nft-staking/programs/nft-staking/src/instructions/mod.rs
@@ -0,0 +1,14 @@
+pub mod initialize;
+pub use initialize::*;
+
+pub mod create_collection;
+pub use create_collection::*;
+
+pub mod mint_asset;
+pub use mint_asset::*;
+
+pub mod stake;
+pub use stake::*;
+
+pub mod unstake;
+pub use unstake::*;
--- nft-staking/programs/nft-staking/src/lib.rs
+++ nft-staking/programs/nft-staking/src/lib.rs
@@ -1,16 +1,40 @@
+#![allow(unexpected_cfgs, deprecated, ambiguous_glob_reexports)]
+
+pub mod error;
+pub mod instructions;
+pub mod state;
+
 use anchor_lang::prelude::*;
+
+pub use instructions::*;
+pub use state::*;
+//pub use error::*;
 
 declare_id!("E73iK9kjm4G99sFNc599JmS9uWPm5nmMXEkm9Ve2hyRL");
 
 #[program]
 pub mod nft_staking {
     use super::*;
+    pub fn initialize(ctx: Context<Initialize>, rewards_bps: u16, freeze_period: u16) -> Result<()> {
+        initialize::handler(ctx, rewards_bps, freeze_period)
+    }
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        msg!("Greetings from: {:?}", ctx.program_id);
-        Ok(())
+    pub fn create_collection(ctx: Context<CreateCollection>, name: String, uri: String) -> Result<()> {
+        create_collection::handler(ctx, name, uri)
+    }
+
+    pub fn mint_assets(ctx: Context<MintAsset>, name: String, uri: String) -> Result<()> {
+        mint_asset::handler(ctx, name, uri)
+    }
+
+    pub fn stake(ctx: Context<Stake>) -> Result<()> {
+        stake::handler(ctx)
+    }
+
+    pub fn unstake(ctx: Context<Unstake>) -> Result<()> {
+        unstake::handler(ctx)
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct Init {}
